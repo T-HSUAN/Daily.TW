ede4b5fe01c0af36d30b6b1a3dfe61cdfd88284c
T-Hsuan <hsuannn.design@gmail.com>
2023-07-13 00:24:31 +0800

    購物車放在同一頁沒有問題了，但我暫時想不到如果放在另一頁怎麼用比較好

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,41 +1,15 @@
 <template>
-    <div class="home">
-        <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-        <HelloWorld :sctionTitle="title" sectionBg="grayGreen" />
-        <HelloWorld sctionTitle="Welcome to Your Vue.js App" sectionBg="gray" />
-        <HelloWorld sctionTitle="sction 2" sectionBg="red" />
+    <div class="about">
+        <h1>This is an about page</h1>
+        <img src="~@/assets/logo.png" alt="" />
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3640.40605792089!2d120.6652766!3d24.1574881!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d784543c9f9%3A0x1e1856ead20202aa!2z5ZyL56uL6Ieq54S256eR5a245Y2a54mp6aSo56ys5LiJ5YGc6LuK5aC077yI6KW_5bGv6Lev77yJ!5e0!3m2!1szh-TW!2stw!4v1688802124512!5m2!1szh-TW!2stw"
+            width="600"
+            height="450"
+            style="border: 0"
+            allowfullscreen="true"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
     </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-    components: {
-        HelloWorld,
-    },
-    created() {
-        fetch("https://api.json-generator.com/templates/GW6mr0Of9PaC/data", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                access_token: "b8wzvaq6cgmkwejpcje480eagic9074mo9rf7hyj",
-            }),
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .then((myJson) => {
-                console.log(myJson);
-            });
-    },
-    data() {
-        return {
-            title: "好想睡一下",
-        };
-    },
-};
-</script>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,8 +1,8 @@
 <style>
-h1{
+h1 {
     padding-left: 100px;
 }
-.title{
+.title {
     display: flex;
     justify-content: center;
     max-width: 1000px;
@@ -11,18 +11,18 @@ h1{
     padding-left: 10px;
     /* border-bottom: 3px solid gray; */
 }
-.title div{
+.title div {
     width: 200px;
     line-height: 30px;
     text-align: center;
 }
-.title .products{
+.title .products {
     text-align: left;
 }
-.title .products{
+.title .products {
     width: 50%;
 }
-.content{
+.content {
     height: 50px;
     margin-left: 137px;
     padding: 25px 0;
@@ -30,45 +30,46 @@ h1{
     align-items: center;
     color: black;
 }
-.content .delete{
+.content .delete {
     height: 100px;
     display: flex;
     align-items: center;
     cursor: pointer;
 }
-.content .delete:hover button, button:hover{
+.content .delete:hover button,
+button:hover {
     color: #a0a2d1;
 }
-.products img{
+.products img {
     width: 80px;
     height: 80px;
     border-radius: 3px;
     margin-left: 15px;
     float: left;
 }
-.products .name{
+.products .name {
     margin-left: 100px;
     margin-top: 20px;
 }
-.count p{
+.count p {
     display: inline-block;
     width: 30px;
     padding: 0 5px;
     text-align: center;
     border: 1px solid gray;
 }
-button{
+button {
     background: none;
     border: none;
     cursor: pointer;
 }
-.result{
+.result {
     padding-right: 250px;
 }
-.result p{
+.result p {
     text-align: right;
 }
-.result .total-price{
+.result .total-price {
     font-size: 32px;
     font-weight: 800;
     color: #d53e3e;
@@ -77,107 +78,85 @@ button{
 <template>
     <main>
         <div class="cart">
-        <h1>購物車</h1>
-        <div class="title">
-            <div class="products">商品</div>
-            <div class="price">單價</div>
-            <div class="count">數量</div> 
-            <div class="amount">金額</div>
-            <div class="delete"></div>
-        </div>
-        <div class="item_list">
-            <div class="title content" v-for="(item, index) in itemList" :key="item.id" >
-                <div class="products">
-                    <img :src="item.img" alt="">
-                    <div class="name">{{item.Name}}</div>
-                </div>                   
-                <div class="price">$ {{item.price}}</div>
-                <div class="count">
-                    <button @click="reduce(item)">-</button>
-                    <p>{{item.count}}</p>
-                    <button @click="add(index)">+</button>
-                </div> 
-                <div class="amount">$ {{item.price * item.count}}</div>
-                <div class="delete" @click="cancel(index)" title="刪除">
-                    <button>X</button>
+            <h1>購物車</h1>
+            <div class="title">
+                <div class="products">商品</div>
+                <div class="price">單價</div>
+                <div class="count">數量</div>
+                <div class="amount">金額</div>
+                <div class="delete"></div>
+            </div>
+            <div class="item_list">
+                <div
+                    class="title content"
+                    v-for="(item, index) in itemList"
+                    :key="item.id"
+                >
+                    <div class="products">
+                        <img :src="item.img" alt="" />
+                        <div class="name">{{ item.Name }}</div>
+                    </div>
+                    <div class="price">$ {{ item.price }}</div>
+                    <div class="count">
+                        <button @click="reduce(item)">-</button>
+                        <p>{{ item.count }}</p>
+                        <button @click="add(index)">+</button>
+                    </div>
+                    <div class="amount">$ {{ item.price * item.count }}</div>
+                    <div class="delete" @click="cancel(index)" title="刪除">
+                        <button>X</button>
+                    </div>
                 </div>
             </div>
+            <div class="result">
+                <p>({{ itemList.length }}項商品) 總計</p>
+                <p>
+                    💰
+                    <span class="total-price"> {{ totalPrice }} </span>
+                    元
+                </p>
+            </div>
         </div>
-        <div class="result">
-            <p>({{itemList.length}}項商品) 總計</p>
-            <p>💰
-                <span class="total-price">
-                    {{totalPrice}} </span>    
-            元</p>
-        </div>
-    </div>
     </main>
 </template>
 <script>
-    export default {
-        data(){
-            return{
-                itemList:[
-                    {
-                        id:1,
-                        Name:'票券A',
-                        img:'https://picsum.photos/300/200/?random=9',
-                        price:500,
-                        count:3
-                    },
-                    {
-                        id:2,
-                        Name:'票券B',
-                        img:'https://picsum.photos/300/200/?random=10',
-                        price:700,
-                        count:6
-                    },
-                    {
-                        id:3,
-                        Name:'票券C',
-                        img:'https://picsum.photos/300/200/?random=11',
-                        price:1200,
-                        count:2
-                    },
-                    {
-                        id:4,
-                        Name:'票券D',
-                        img:'https://picsum.photos/300/200/?random=12',
-                        price:2300,
-                        count:1
-                    }
-                ]
+export default {
+    data() {
+        return {
+            itemList: [],
+        };
+    },
+    methods: {
+        //三種寫法
+        add(index) {
+            this.itemList[index].count++;
+        },
+        reduce(item) {
+            if (item.count > 0) {
+                item.count--;
             }
         },
-        methods:{//三種寫法
-            add(index){
-                this.itemList[index].count++;
-            },
-            reduce(item){
-                if(item.count>0){
-                item.count--;                
-                }
-            },
-            cancel: function(index){
-                console.log(this);
-                this.itemList.splice(index,1);
-            }
+        cancel: function (index) {
+            console.log(this);
+            this.itemList.splice(index, 1);
         },
-        computed:{
-            totalPrice(){
-                if(this.itemList.length === 0) return
-                let total = 0
-                for (let index = 0; index < this.itemList.length; index++) {
-                    const accumulator = this.itemList[index].price * this.itemList[index].count
-                    total += accumulator 
-                }
-                return total;
+    },
+    computed: {
+        totalPrice() {
+            if (this.itemList.length === 0) return;
+            let total = 0;
+            for (let index = 0; index < this.itemList.length; index++) {
+                const accumulator =
+                    this.itemList[index].price * this.itemList[index].count;
+                total += accumulator;
             }
-            // totalPrice() {
-            //     return this.itemList.reduce((total, item) => total + (item.price * item.count), 0);
-            // }
-            //this 代表 vue 實體本身，total 表示 reduce() 中每次累加的結果，item 是 itemList 每一個元素
-            //箭頭函式是用於處理陣列中每個元素的函式，0 為第一次呼叫函式時的累加器初始值。
-        }
-    }
+            return total;
+        },
+        // totalPrice() {
+        //     return this.itemList.reduce((total, item) => total + (item.price * item.count), 0);
+        // }
+        //this 代表 vue 實體本身，total 表示 reduce() 中每次累加的結果，item 是 itemList 每一個元素
+        //箭頭函式是用於處理陣列中每個元素的函式，0 為第一次呼叫函式時的累加器初始值。
+    },
+};
 </script>
