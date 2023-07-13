c59a6766730dab596eee36022d2f16970e1a5d76
BenjaminChien <ben19990616@gmail.com>
2023-07-13 14:59:01 +0800

    install font-awesome

--- src/main.js
+++ src/main.js
@@ -12,4 +12,17 @@ import store from "./store";
 // Vue.use(BootstrapVue)
 // Vue.use(BootstrapVueIcons)
 
-createApp(App).use(store).use(router).mount("#app");
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount("#app");
