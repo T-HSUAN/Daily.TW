b419740fdd5123a259076b781a0d8fc426611e85
BenjaminChien <ben19990616@gmail.com>
2023-07-14 21:33:24 +0800

    add layout ticket vertical

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <MainHeader></MainHeader>
     <btn :btnTexts="你想要參照的key"></btn>
     <main>
-
+       <TicketVertical></TicketVertical>
       <router-view />
     </main>
   
@@ -12,12 +12,14 @@
 <script>
 import MainHeader from "@/components/MainHeader.vue";
 import MainFooter from "@/components/MainFooter.vue";
+import TicketVertical from "@/components/TicketVertical.vue";
 
 
 export default {
   components: {
     MainHeader,
     MainFooter,
+    TicketVertical
 },
 };
 </script>
