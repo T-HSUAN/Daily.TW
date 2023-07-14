93d77098e61771fae2b0ec7a802ecae64729b8fb
wangmtsung <wjt5218703@gmail.com>
2023-07-14 20:11:13 +0800

    增加更多mixin

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,9 +1,25 @@
 <template>
     <header>
-        <div class="header">
+        <div class="header" v-show="showHeader">
             <!-- <img alt="logo" src="./assets/img/logo.png"> -->
             <router-link to="/">
-                <img :src="require('@/assets/img/logo.png')" alt="logo" />
+                <img :src="require('../assets/img/big.svg')" alt="logo" />
+            </router-link>
+            <nav>
+                <router-link to="/plan">專屬旅著</router-link>
+                <router-link to="/trip">出遊特輯</router-link>
+                <router-link to="/oott">穿搭特輯</router-link>
+                <router-link to="/ticket">景點票券</router-link>
+                <router-link to="/about">關於我們</router-link>
+                <router-link to="/login">登入|註冊</router-link>
+                <!-- <router-link to="/cart">購物車</router-link> -->
+            </nav>
+        </div>
+    
+        <div class="header_sm" v-show=" !showHeader">
+            <!-- <img alt="logo" src="./assets/img/logo.png"> -->
+            <router-link to="/">
+                <img :src="require('../assets/img/small.svg')" alt="logo" />
             </router-link>
             <nav>
                 <router-link to="/plan">專屬旅著</router-link>
@@ -19,14 +35,27 @@
 </template>
 <script>
 export default {
-    data() {
-        return {
-            logo: "logo.png",
-        };
-    },
-    methods: {},
+  data() {
+    return {
+      showHeader: true
+    };
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.showHeader = window.scrollY <= 0;
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 @import "@/assets/scss/main.scss";
 </style>
+
+
+
