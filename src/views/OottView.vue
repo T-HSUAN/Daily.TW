04e784af8c7aa5f9cef794071fae5b0271536014
wangmtsung <wjt5218703@gmail.com>
2023-07-18 14:54:33 +0800

    小改

--- src/views/OottView.vue
+++ src/views/OottView.vue
@@ -4,6 +4,16 @@
 </style>
 
 <template>
+    <div class="post_section">
+        <div class="post_img"><img :src="require('@/assets/img/oott_view_first.png')" alt="post"></div>
+        <div class="post_img">
+            <img :src="require('@/assets/img/oott_view_second.png')" alt="post">
+            <h1>穿搭投稿徵集中</h1>
+            <p>跟我們分享你的 #oott 吧！</p>
+            
+        </div>
+
+    </div>
 </template>
 
 <script>
