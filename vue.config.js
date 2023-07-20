fb02bedb9724d57837fc9dd8be72b43968d7eaf2
T-Hsuan <hsuannn.design@gmail.com>
2023-07-21 02:27:55 +0800

    網頁標籤icon&title設置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  filenameHashing: false,
-  css: {
-    sourceMap: true
-  },
-})
+    transpileDependencies: true,
+    filenameHashing: false,
+    css: {
+        sourceMap: true,
+    },
+    chainWebpack: (config) => {
+        config.plugin("html").tap((args) => {
+            args[0].title = `Daily.TW`;
+            return args;
+        });
+    },
+});
