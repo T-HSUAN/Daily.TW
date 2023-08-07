5c021fb231771599f4016eb3c625c9622cc80709
BenjaminChien <ben19990616@gmail.com>
2023-08-07 21:15:17 +0800

    改變 connectDailyTW 的路徑，改路徑為我們的資料庫

--- public/phpfile/test.php
+++ public/phpfile/test.php
@@ -6,7 +6,7 @@ try {
 	require_once("connectDailyTW.php");
 	
 	//執行sql指令並取得pdoStatement
-	$sql = "select * from products";
+	$sql = "select * from trip";
 	$products = $pdo->query($sql); 
 	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
   echo json_encode($prodRows);
