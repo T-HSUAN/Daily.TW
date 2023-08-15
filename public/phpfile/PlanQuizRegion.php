0dc508ea782ca0382a55856a61f91471567b93d9
raccoonoob <susan926926@gmail.com>
2023-08-16 03:55:12 +0800

    專屬旅著結果頁串資料庫顯示景點資料及圖片輪播、地區對應的天氣預報

--- public/phpfile/PlanQuizRegion.php
+++ public/phpfile/PlanQuizRegion.php
@@ -6,7 +6,7 @@ try {
 	require_once("connectDailyTW.php");
 	
 	//執行sql指令並取得pdoStatement
-	$sql = "select * from region";
+	$sql = "SELECT * FROM region WHERE NOT region_name = '桃園';";
 	$products = $pdo->query($sql); 
 	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($prodRows);
