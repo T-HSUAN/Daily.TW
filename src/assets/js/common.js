7b3193b5b945d8341aed176100a1e59827872029
raccoonoob <susan926926@gmail.com>
2023-08-17 09:43:48 +0800

    路徑改回localhost

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,4 +1,4 @@
-// export const URL = 'http://localhost/Daily.TW/public/phpfile';
-export const URL = 'https://tibamef2e.com/chd102/g5/phpfile';
-// export const URL_MAC = 'http://localhost:8080/public/phpfile';
-export const URL_MAC = 'https://tibamef2e.com/chd102/g5/phpfile';
+export const URL = 'http://localhost/DailyTW/public/phpfile';
+// export const URL = 'https://tibamef2e.com/chd102/g5/phpfile';
+export const URL_MAC = 'http://localhost:8080/public/phpfile';
+// export const URL_MAC = 'https://tibamef2e.com/chd102/g5/phpfile';
